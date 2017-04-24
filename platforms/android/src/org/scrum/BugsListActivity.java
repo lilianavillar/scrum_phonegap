0461e0fd389cfc01b9ac9c56b6b34f29550e7df3
lilianavillar <lilianavillar@hotmail.es>
2017-04-24 17:01:45 +0200

    QR

--- platforms/android/src/org/scrum/BugsListActivity.java
+++ platforms/android/src/org/scrum/BugsListActivity.java
@@ -1,15 +1,28 @@
 package org.scrum;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -19,6 +32,10 @@ import org.json.JSONObject;
 import org.scrum.util.Bug;
 import org.scrum.util.BugAdapter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,11 +43,13 @@ import cz.msebera.android.httpclient.Header;
 
 public class BugsListActivity extends AppCompatActivity {
 
+    private static final int PERMISO_EXTERNAL = 1 ;
     private List<Bug> bugsList;
     private ListView listView;
     private CoordinatorLayout coordinator;
     private String sprintName;
     private String sprintPasswd;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +59,15 @@ public class BugsListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         this.listView = (ListView) findViewById(R.id.bugsList);
         this.coordinator = (CoordinatorLayout) findViewById(R.id.coordinatorBugsList);
+        this.progressBar = (ProgressBar) findViewById(R.id.cargandoLista);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        if (ContextCompat.checkSelfPermission(this,  Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISO_EXTERNAL);
             }
-        });
+        }
 
         Intent intent= this.getIntent();
         if (intent.hasExtra("items")){
@@ -56,23 +75,33 @@ public class BugsListActivity extends AppCompatActivity {
             this.sprintName = valores[0];
             this.sprintPasswd = valores[1];
         }
-
         crearBugsList();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISO_EXTERNAL: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                }
+            }
+        }
     }
 
     private void crearBugsList() {
 
         bugsList = new ArrayList<Bug>();
         AsyncHttpClient client = new AsyncHttpClient();
-
         client.get("https://appscrum.herokuapp.com/bugs/" + sprintName, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 if(response.length() >= 1){
                     try {
-                        JSONObject objeto = (JSONObject) response.get(0);
-                        if(objeto.has("bugs")){
-                            JSONArray bugs = objeto.getJSONArray("bugs");
+                        JSONObject respuesta = (JSONObject) response.get(0);
+                        if(respuesta.has("bugs")){
+                            JSONArray bugs = respuesta.getJSONArray("bugs");
                             for (int i=0; i<bugs.length();i++){
                                 Bug bug = new Bug();
                                 bug.setTitulo(bugs.getJSONObject(i).getString("titulo"));
@@ -84,6 +113,7 @@ public class BugsListActivity extends AppCompatActivity {
                             }
                             BugAdapter adapter = new BugAdapter(getApplicationContext(), bugsList);
                             listView.setAdapter(adapter);
+                            progressBar.setVisibility(View.GONE);
                         }
 
                     } catch (JSONException e) {
@@ -103,5 +133,7 @@ public class BugsListActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
 }
--- platforms/android/src/org/scrum/util/BugAdapter.java
+++ platforms/android/src/org/scrum/util/BugAdapter.java
@@ -1,14 +1,33 @@
 package org.scrum.util;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 import org.scrum.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -16,6 +35,7 @@ import java.util.List;
  */
 
 public class BugAdapter extends BaseAdapter{
+
 
     private Context context;
     private List<Bug> bugs;
@@ -53,6 +73,8 @@ public class BugAdapter extends BaseAdapter{
         TextView estado = (TextView) rowView.findViewById(R.id.itemEstado);
         TextView prioridad = (TextView) rowView.findViewById(R.id.itemPrioridad);
         TextView estimacion = (TextView) rowView.findViewById(R.id.itemEstimacion);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.imageBug);
+
         Bug bug = this.bugs.get(position);
         titulo.setText("Título: " + bug.getTitulo().toString());
         descripcion.setText("Descripcion: " + bug.getDescripcion().toString());
@@ -60,6 +82,78 @@ public class BugAdapter extends BaseAdapter{
         prioridad.setText("Prioridad: " + bug.getPrioridad());
         estimacion.setText("Estimación: " + bug.getEstimacion());
 
+        Gson gson = new Gson();
+        String json = gson.toJson(bug);
+
+        Bitmap imagen = createQR(json.toString());
+        saveImage(imagen,  bug.getTitulo().toString());
+        imageView.setImageBitmap(imagen);
+
         return rowView;
+    }
+
+    private Bitmap createQR(String text) {
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            return bmp;
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void saveImage(Bitmap ImageToSave, String titulo) {
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/bugs";
+        File dir = new File(file_path);
+
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        File file = new File(dir, titulo + ".jpg");
+
+        try {
+            FileOutputStream fOut = new FileOutputStream(file);
+
+            ImageToSave.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+            makeSureFileWasCreatedThenMakeAvabile(file);
+            galleryAddPic(file_path + "/" + titulo + ".jpg");
+        }
+
+        catch(FileNotFoundException e) {
+
+        }
+        catch(IOException e) {
+        }
+
+    }
+
+    private void galleryAddPic(String path) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(path);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        context.sendBroadcast(mediaScanIntent);
+    }
+
+    private void makeSureFileWasCreatedThenMakeAvabile(File file){
+        MediaScannerConnection.scanFile(context,
+                new String[] { file.toString() } , null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+
+                    public void onScanCompleted(String path, Uri uri) {
+                    }
+                });
     }
 }
